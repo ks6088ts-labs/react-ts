2b63db8c1ca8cd17fa413e2a15d237ad5b025627
ks6088ts <ks6088ts@gmail.com>
2022-11-29 09:58:06 +0900

    update eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
     ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+        '@typescript-eslint/explicit-module-boundary-types': 0,
+        'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
